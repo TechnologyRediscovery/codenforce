e1febd959b7c76e88fa60a07e29998d858060db0
Eric Darsow <ericdarsow@gmail.com>
2019-04-11 17:59:17 -0400

    case profile report work in progress

--- codeconnect/database/patches/dbpatch_beta9.sql
+++ codeconnect/database/patches/dbpatch_beta9.sql
@@ -4,6 +4,30 @@ ALTER TABLE municipality ADD COLUMN defaultcourtentity INTEGER CONSTRAINT muni_d
 ALTER TABLE loginmuni ADD COLUMN default BOOLEAN DEFAULT FALSE;
 ALTER TABLE ceevent ADD COLUMN directrequesttodefaultmuniceo boolean DEFAULT false;
 
+-- Add icon ids to ceeeventcategories
+
+
+--
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (10, 'PrelimInvestigationPending');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (10, 'NoticeDelivery');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (11, 'InitialComplianceTimeframe');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (11, 'SecondaryComplianceTimeframe');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (12, 'AwaitingHearingDate');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (12, 'HearingPreparation');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (12, 'InitialPostHearingComplianceTimeframe');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (12, 'SecondaryPostHearingComplianceTimeframe');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (14, 'InactiveHolding');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (13, 'Closed');
+INSERT INTO public.cecasestatusicon (iconid, status) VALUES (14, 'LegacyImported');
+
+
+-- add foreign keys to 
+
+--- ceevent categories
+
+ALTER TYPE persontype ADD VALUE IF NOT EXISTS 'User' BEFORE 'CogStaff'S; 
+
+
 
 
 
