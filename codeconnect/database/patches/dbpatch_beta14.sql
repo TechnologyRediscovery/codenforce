90c04103ad745d8bab727876fe485bccc6c0b0a6
Eric Darsow <ericdarsow@gmail.com>
2019-05-30 13:48:20 -0400

    turned dbpatch 13 into dominic's occ updates

---
+++ codeconnect/database/patches/dbpatch_beta14.sql
@@ -0,0 +1,36 @@
+
+-- have not wired this new column up yet!
+ALTER TABLE ceactionrequest ADD COLUMN usersubmitter_userid INTEGER;
+ALTER TABLE ceactionrequest ADD CONSTRAINT ceactionreq_usersub_fk FOREIGN KEY (usersubmitter_userid) REFERENCES login (userid);
+
+-- add stipulated compliance that freeze in time when added to a notice of violation
+
+
+CREATE SEQUENCE IF NOT EXISTS occperiodid_seq
+	START WITH 1000
+	INCREMENT BY 1
+	MINVALUE 1000
+	NO MAXVALUE 
+	CACHE 1;
+
+CREATE TABLE public.occperiod
+(
+
+	periodid 						INTEGER DEFAULT nextval('occperiodid_seq') NOT NULL,
+	propertyunit_unitid
+	startdate						TIMESTAMP WITH TIME ZONE,
+	startdatecertifiedby_userid
+	startdatecertifiedts
+	enddate
+	enddatecertifiedby_userid
+	enddatecterifiedts
+	manager_userid
+	
+
+) ;
+
+
+INSERT INTO public.dbpatch(
+            patchnum, patchfilename, datepublished, patchauthor, notes)
+    VALUES (13, 'database/patches/dbpatch_beta13.sql', '', 'ecd', 'final clean ups');
+
