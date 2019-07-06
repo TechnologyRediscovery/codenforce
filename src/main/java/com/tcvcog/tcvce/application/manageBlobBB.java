f1eede02dcc17c57a0136d81eea36d7177ddb15d
Noah <nbaertsch@github.com>
2019-07-06 16:36:19 -0400

    okay i really added it now

--- src/main/java/com/tcvcog/tcvce/application/manageBlobBB.java
+++ src/main/java/com/tcvcog/tcvce/application/manageBlobBB.java
@@ -7,22 +7,12 @@ package com.tcvcog.tcvce.application;
 
 import com.tcvcog.tcvce.domain.IntegrationException;
 import com.tcvcog.tcvce.entities.Blob;
-import com.tcvcog.tcvce.entities.BlobType;
 import com.tcvcog.tcvce.integration.BlobIntegrator;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -30,10 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class manageBlobBB extends BackingBeanUtils implements Serializable{
     
-     private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
-     
     private List<Blob> blobList;
-    private Blob selectedBlob;
 
     /**
      * Creates a new instance of manageBlobBB
@@ -51,53 +38,8 @@ public class manageBlobBB extends BackingBeanUtils implements Serializable{
         try {
             this.blobList = getBlobIntegrator().getRecentBlobs();
         } catch (IntegrationException ex) {
-            System.out.println("manageBlobBB.initBean | " + ex + "\n" + ex.getException().getLocalizedMessage());
+            System.out.println("manageBlobBB.initBean | " + ex);
         }
-    }
-    
-    public void downloadBlob(Blob blob){
-
-        // Prepare.
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        ExternalContext externalContext = facesContext.getExternalContext();
-        HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
-
-        BufferedInputStream input = null;
-        BufferedOutputStream output = null;
-
-        try {
-            // Open file.
-            input = new BufferedInputStream(new ByteArrayInputStream(blob.getBytes()));
-
-            // Init servlet response.
-            response.reset();
-            if(blob.getType() == BlobType.PDF)
-                response.setHeader("Content-Type", "application/pdf");
-            else
-                response.setHeader("Content-Type", "image/png");
-            response.setHeader("Content-Length", String.valueOf(blob.getBytes().length));
-            response.setHeader("Content-Disposition", "inline; filename=\"" + blob.getFilename() + "\"");
-            output = new BufferedOutputStream(response.getOutputStream(), DEFAULT_BUFFER_SIZE);
-
-            // Write file contents to response.
-            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-            int length;
-            while ((length = input.read(buffer)) > 0) {
-                output.write(buffer, 0, length);
-            }
-
-            // Finalize task.
-            output.flush();
-        } catch (IOException ex) {
-            System.out.println("manageBlobBB.downloadPDF | " + ex);
-        } finally {
-            // close streams.
-            if (output != null){  try { output.close(); } catch (IOException ex) { /* Ignore */ } }
-            if (input != null){  try { input.close(); } catch (IOException ex) { /* Ignore */ } }
-        }
-
-        // Inform JSF that it doesn't need to handle response.
-        facesContext.responseComplete();
     }
 
     /**
@@ -112,20 +54,6 @@ public class manageBlobBB extends BackingBeanUtils implements Serializable{
      */
     public void setBlobList(List<Blob> blobList) {
         this.blobList = blobList;
-    }
-
-    /**
-     * @return the selectedBlob
-     */
-    public Blob getSelectedBlob() {
-        return selectedBlob;
-    }
-
-    /**
-     * @param selectedBlob the selectedBlob to set
-     */
-    public void setSelectedBlob(Blob selectedBlob) {
-        this.selectedBlob = selectedBlob;
     }
     
 }
