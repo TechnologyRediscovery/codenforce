bbc72b36a6c0b60391c90c8e7631246c0f16fc5b
Snapper <LearningWithSnapper@gmail.com>
2020-06-06 17:12:46 -0400

    Fixed exception docstrings

--- codeconnect/scripts/deployedScriptssetversion0.9/_exceptions.py
+++ codeconnect/scripts/deployedScriptssetversion0.9/_exceptions.py
@@ -4,21 +4,6 @@ error_logger.setLevel(logging.DEBUG)
 
 error_logger.debug('Error logger initialized')
 
-def log_error(error, parcelID):
-    if error.subtype:
-        error_logger.warning(
-            "Malformed %s at parcel ID %s: %s could not be parsed",
-            error.type,
-            parcelID,
-            error.subtype,
-            exc_info=True
-        )
-    else:
-        error_logger.warning(
-            "Malformed %s at parcel ID %s", error.type, parcelID, exc_info=True
-        )
-
-
 
 # Todo: Consider abstract base class implementation
 class MalformedDataError(IndexError):
@@ -30,23 +15,26 @@ class MalformedDataError(IndexError):
 # Address Errors
 # ------------------------------
 
-
 class MalformedAddressError(MalformedDataError):
-    """ Thrown when scraped address HTML doesn't fit expected format """
+    """
+    Thrown when scraped address HTML doesn't fit expected format and the error can't be further specified.
+    Further subclassed by more specific Address Errors.
+    """
     def __init__(self):
         self.type = 'address'
         self.subtype = None
 
+
 # Todo: Implement behavior using metaclasses to ensure attributes exist
 class MalformedStateError(MalformedAddressError):
-    """ Thrown when the program can't parse State information """
+    """ Thrown when the script can't parse State information. """
     def __init__(self):
         super().__init__()
         self.subtype = 'state'
 
 
 class MalformedZipcodeError(MalformedAddressError):
-    """ Thrown when the program can't parse zipcode information """
+    """ Thrown when the script can't parse zipcode information. """
     def __init__(self):
         super().__init__()
         self.subtype = 'zipcode'
@@ -54,7 +42,7 @@ class MalformedZipcodeError(MalformedAddressError):
 
 
 class MalformedLotAndBlockError(MalformedAddressError):
-    """ Thrown when the program can't parse lot and block information """
+    """ Thrown when the script can't parse lot and block information. """
     def __init__(self):
         super().__init__()
         self.subtype = 'lot and block'
@@ -64,11 +52,8 @@ class MalformedLotAndBlockError(MalformedAddressError):
 # Owner Errors
 # ------------------------------
 
-
-
-
 class MalformedOwnerError(MalformedDataError):
-    """ Thrown when scraped address HTML doesn't fit expected format"""
+    """ Thrown when scraped person (owner) HTML doesn't fit expected format. """
 
     def __init__(self):
         self.type = 'owner'
