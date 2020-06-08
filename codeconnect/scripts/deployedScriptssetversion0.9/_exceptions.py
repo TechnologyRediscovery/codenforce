03f3693f96d03626bcd02aadf14e06f1578c0ec8
Snapper <LearningWithSnapper@gmail.com>
2020-06-08 12:06:15 -0400

    Better commenting. Blacked.

--- codeconnect/scripts/deployedScriptssetversion0.9/_exceptions.py
+++ codeconnect/scripts/deployedScriptssetversion0.9/_exceptions.py
@@ -7,12 +7,12 @@ import sys
 error_logger = logging.getLogger(__name__)
 error_logger.setLevel(logging.DEBUG)
 
-error_logger.debug('Error logger initialized')
+error_logger.debug("Error logger initialized")
 
 
-# Todo: Consider abstract base class implementation
 class MalformedDataError(IndexError):
     """ Base class for errors resulting from Malformed Data """
+
     def __init__(self):
         self.error_code = 0
 
@@ -21,6 +21,7 @@ class MalformedDataError(IndexError):
 # Address Errors
 # ------------------------------
 
+
 class MalformedGenericAddressError(MalformedDataError):
     """
     Thrown when scraped address HTML doesn't fit expected format and the error can't be further specified.
@@ -28,19 +29,18 @@ class MalformedGenericAddressError(MalformedDataError):
     """
 
     def __init__(self):
-        self.type = 'address'
+        self.type = "address"
         self.error_code = 1
         self.subtype = None
 
 
-# Todo: Implement behavior using metaclasses to ensure attributes exist
 class MalformedStateError(MalformedGenericAddressError):
     """ Thrown when the script can't parse State information. """
 
     def __init__(self):
         super().__init__()
         self.error_code = 2
-        self.subtype = 'state'
+        self.subtype = "state"
 
 
 class MalformedZipcodeError(MalformedGenericAddressError):
@@ -49,7 +49,7 @@ class MalformedZipcodeError(MalformedGenericAddressError):
     def __init__(self):
         super().__init__()
         self.error_code = 3
-        self.subtype = 'zipcode'
+        self.subtype = "zipcode"
 
 
 class MalformedLotAndBlockError(MalformedGenericAddressError):
@@ -58,7 +58,7 @@ class MalformedLotAndBlockError(MalformedGenericAddressError):
     def __init__(self):
         super().__init__()
         self.error_code = 4
-        self.subtype = 'lot and block'
+        self.subtype = "lot and block"
 
 
 # Todo: Find out what can break to throw this error. Currently not implemented
@@ -68,18 +68,19 @@ class MalformedStreetError(MalformedGenericAddressError):
     def __init__(self):
         super().__init__()
         self.error_code = 5
-        self.subtype = 'street'
+        self.subtype = "street"
 
 
 # ------------------------------
 # Owner Errors
 # ------------------------------
 
+
 class MalformedOwnerError(MalformedDataError):
     """ Thrown when scraped person (owner) HTML doesn't fit expected format. """
 
     def __init__(self):
-        self.type = 'owner'
+        self.type = "owner"
         self.error_code = 6
         self.subtype = None
 
@@ -88,17 +89,25 @@ class MalformedOwnerError(MalformedDataError):
 # Code Validation
 # ------------------------------
 # Note: Code introspection is a haphazard approach to code validation.
-# I would use metaclasses if I understood them well enough.
-# Unfortunately, this code is likely to stick around. Although it's bad practice, the code validation is effective.
+# However, the code works, and is heavily commented so the more "clever" approaches can be understood
+# Feel free to rewrite the code using metaclasses (the 'proper' way to do code validation) if it bothers you too much 😛
 
-# Creates a dictionary where keys are the error code and values are the exception class
+# The following chunk of codes creates the dictionary errorcode_lookup where
+#   The key is an error code
+#   The value is a list of exception names that share the error code
+# It is not in a function so that inspect.getmembers is in the same scope as the error classes
+
+# custom_exceptions is a list of tuples where
+#   The zeroth index of the tuple is the name of the error code
+#   The first index of the tuple is the exception class
 custom_exceptions = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-
 # It is important we import defaultdict after saving our custom_exceptions.
 # Otherwise, it will erroneously be added to the list of exceptions
 from collections import defaultdict
 
 errorcode_lookup = defaultdict(list)
+
+# This loop creates the dictionary.
 for exception in custom_exceptions:
     exception_name = exception[0]
     exception_class = exception[1]
@@ -110,15 +119,15 @@ for exception in custom_exceptions:
         errorcode_lookup[e.error_code].append(exception_name)
 
 # Validates that no two errorcodes are the same
+collision_flag = False
 for key in errorcode_lookup:
     err_name_list = errorcode_lookup[key]
-    if len(err_name_list) == 0:
+    if len(err_name_list) > 1:
         print(
-            'ERROR: {} does not have a corresponding error code'.format(err_name_list[0])
+            "ERROR: error code {} is shared by the following exceptions: {}".format(
+                key, err_name_list
+            )
         )
-        raise ValueError
-    elif len(err_name_list) > 1:
-        print(
-            'ERROR: error code {} is shared by the following exceptions: {}'.format(key, err_name_list)
-        )
-        raise ValueError
+        collision_flag = True
+if collision_flag:
+    raise ValueError
