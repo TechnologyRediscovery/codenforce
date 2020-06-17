766b18ccfbdf25b25f30ac81ebafeac18afe33b8
Snapper <LearningWithSnapper@gmail.com>
2020-06-17 01:01:16 -0400

    get_county_property_assessment now returns 3 pages' html. create_insertmap_from_html has been both updated and refactored to reflect this change. _create_insertmap created to dramatically cut down code reuse. get_county_property_assesment has been moved from scraping_and_parsing to update_script. scraping_and_parsing is now just parsing.

--- codeconnect/snapper's_scripts/UpdatePropertyExternalData/update_script.py
+++ codeconnect/snapper's_scripts/UpdatePropertyExternalData/update_script.py
@@ -6,27 +6,70 @@ If the data is different, creates writes an event to the event table
 
 from collections import namedtuple
 
-from Util.db_conn import get_cursor
-from Util.scraping_and_parsing import (
-    get_county_property_assessment,
-    soupify_html,
-    extract_info_from_soup,
-    extract_address_parts,
-)
+import requests
 
-from Exceptions._exceptions import MalformedDataError
+from Util.db_conn import get_cursor
+from Util.parsing import (
+    create_general_insertmap,
+    create_building_insertmap,
+    create_tax_insertmap
+)
 
 
 def main():
     rows = ask_database_for_parcelids(municode=828)   # Todo: Better naming? It's property and parcel id.
     for row in rows:
 
-        raw_html = get_county_property_assessment(row.parcel_id)
-        insert_map = create_insertmap_from_html(raw_html)
+        html_dict = get_county_property_assessment(row.parcel_id)
+        insert_map = create_insertmap_from_html(html_dict, row.property_id)
         written_data = write_data_to_propertyexternaldata(insert_map)
         did_the_data_change = check_if_data_is_different_than_previous(written_data)
         if did_the_data_change:
             create_event()
+
+# MAGIC STRINGS
+GENERALINFO = "GeneralInfo"
+BUILDING = "Building"
+TAX = "Tax"
+
+def get_county_property_assessment(parcel_id):
+    """
+    Grabs the raw HTML of a property from the county's website
+
+    Arguments:
+        parcel_id: str
+
+    Returns:
+        dict[str]
+            A dictionary where the key is the scraped page's name and the value is the raw html
+    """
+    COUNTY_REAL_ESTATE_URL = (
+        "http://www2.county.allegheny.pa.us/RealEstate/"
+    )
+    pages = {
+        GENERALINFO: None,
+        BUILDING: None,
+        TAX: None
+    }
+    URL_ENDING = ".aspx?"
+
+    search_parameters = {
+        "ParcelID": parcel_id,
+        "SearchType": 3,
+        "SearchParcel": parcel_id,
+    }
+
+    print("Scraping data from county: " + parcel_id)
+    for page in pages:
+        try:
+            response = requests.get(
+                (COUNTY_REAL_ESTATE_URL + page + URL_ENDING), params=search_parameters, timeout=5
+            )
+            pages[page] = response.text
+        except requests.exceptions.Timeout:
+            # TODO: log_error(). Perhaps logger should reside in its own auxiliary module?
+            raise requests.exceptions.Timeout
+    return pages
 
 
 def ask_database_for_parcelids(municode=None, muniname=None):
@@ -56,57 +99,50 @@ def ask_database_for_parcelids(municode=None, muniname=None):
         yield Row(*row)  # Casts each tuple returned by the cursor to the namedtuple 'Row'
 
 
-db_to_span= {
-    # Todo: Document and find proper place in code to put this
-    # Consider not using magic strings?
-    "ownername": "BasicInfo1_lblOwner",
-    "fulladdress": "BasicInfo1_lblAddress",
-    "ownermailing": "lblChangeMail",
-    "saleprice": "lblSalePrice",
-    "saledate": "lblSaleDate",      # -> saleyear
-    "assessedlandvalue": "lblCountyLand",
-    "assessedbuildingvalue": "lblCountyBuild",
-    "assessmentyear": "LCounty", # re.numbers
-    "usecode": "lblUse",
-}
-
-
-def create_insertmap_from_html(raw_html):
+def create_insertmap_from_html(html_dict, property_id):
     """
     Arguments:
-          raw_html: str
-          The raw HTML scraped from the county website
+        html_dict: str
+            The raw HTML scraped from the county website
+        property_id: int
+            CodeConnect's primary key for property
 
     Returns:
         Dict
-            A dictionary of information extracted from the HTML
+            A dictionary of information extracted from the HTML. Mostly corresponds to the table propertyexternaldata
     """
-    imap = {}   # insertmap
-    soup = soupify_html(raw_html)
-    for key in db_to_span:
-        # NOTE: ownername may be a string or a list
-        imap[key] = extract_info_from_soup(soup, db_to_span[key])
+    imap = {}
+    # If we were scraping more, I would iterate over these in a loop, and have the function
+    general_map = create_general_insertmap(html_dict[GENERALINFO])
+    building_map = create_building_insertmap(html_dict[BUILDING])
+    tax_map = create_tax_insertmap(html_dict[TAX])
 
-    # TODO: IMPORTANT: Add Try/Except MalformedDataError
-    address_map = extract_address_parts(imap['fulladdress'])
-    imap.update(address_map)
+    imap.update(general_map)
+    imap.update(building_map)
+    imap.update(tax_map)
 
-    for key in [
-        "saleprice", "saledate", "assessedlandvalue", "assessedbuildingvalue",
-    ]:
-        imap[key] = imap[key].lstrip('$')
-    imap["saleyear"] = imap["saledate"][-4:]
-    imap["assessmentyear"] = imap["assessmentyear"][:4]
+    imap["prop_id"] = property_id
     print(imap)
     return imap
 
 
 def write_data_to_propertyexternaldata(insert_map):
     """
-
-    The columns ownerphone and yearbuilt cannot be found by scraping the county site
+    Writes data to the table propertyexternaldata. Ran whenever
+    ownerphone cannot be found by scraping the county site and are excluded from the insert:
     """
-    pass
+    insert_sql = """
+        INSERT INTO public.propertyexternaldata(
+            property_propertyid, ownername, address_street, address_citystatezip,
+            address_city, address_state, address_zip, saleprice,
+            saleyear, assessedlandvalue, assessedbuildingvalue, assessmentyear,
+            usecode, livingarea, condition, taxstatus,
+            taxstatusyear, notes, lastupdated
+        )
+        VALUES(
+        
+        )
+    """
 
 
 def check_if_data_is_different_than_previous(written_data):
