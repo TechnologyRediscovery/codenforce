8356752f2ddf0355c624c18a0095ecbd26895c8e
Snapper <LearningWithSnapper@gmail.com>
2020-07-16 13:22:16 -0400

    Integrating scripts git submodule into project -- Amended: Fixed submodule

--- codeconnect/scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This directory will eventually replace scripts.
-Creating a new work environment allows organized code from the start.
-
-Whether the code maintains its organization remains to be seen (haha)
-"""
