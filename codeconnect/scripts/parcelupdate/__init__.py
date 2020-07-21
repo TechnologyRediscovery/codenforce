44de154e932c653ea5eb14299f4b6091618e4439
Snapper <LearningWithSnapper@gmail.com>
2020-07-20 20:20:50 -0400

    Removed deprecated files from __all__. Added __author/email__, still unused.

--- codeconnect/scripts/parcelupdate/__init__.py
+++ codeconnect/scripts/parcelupdate/__init__.py
@@ -1,15 +1,15 @@
 __all__ = [
     "_constants",
-    "_db_conn",
     "_scrape_and_parse",
     "_events",
     "_fetch",
     "parcelupdate",
     "_insert",
-    "_property"
 ]
 
 #   parcelupdate uses semantic versioning (https://semver.org/)
 #   Please note that the versioning will not be kept up to date until version 1.0.0
 VERSION = (0, 0, 1)
 __version__ = '.'.join(map(str, VERSION))
+__author__ = "Snapper Vibes"
+__email__ = "LearningWithSnapper@gmail.com"
