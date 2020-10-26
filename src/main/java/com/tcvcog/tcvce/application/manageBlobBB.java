4acc0a1836df4b4888a070f47841fb6c4778e1c2
NADGIT <nad100@pitt.edu>
2020-10-26 18:26:17 -0400

    Began work on blob management page

--- src/main/java/com/tcvcog/tcvce/application/manageBlobBB.java
+++ src/main/java/com/tcvcog/tcvce/application/manageBlobBB.java
@@ -1,13 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2020 Technology Rediscovery LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.tcvcog.tcvce.application;
 
 import com.tcvcog.tcvce.domain.IntegrationException;
 import com.tcvcog.tcvce.entities.Blob;
+import com.tcvcog.tcvce.entities.BlobLight;
 import com.tcvcog.tcvce.entities.BlobType;
+import com.tcvcog.tcvce.entities.PageModeEnum;
 import com.tcvcog.tcvce.integration.BlobIntegrator;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -17,6 +30,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
@@ -29,32 +43,37 @@ public class manageBlobBB extends BackingBeanUtils implements Serializable{
     
      private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
      
-    private List<Blob> blobList;
+    private PageModeEnum currentMode;
+    private List<PageModeEnum> pageModes;
+     
+    private List<BlobLight> blobList;
     private Blob selectedBlob;
-
-    /**
-     * Creates a new instance of manageBlobBB
-     */
-    public manageBlobBB() {
-        
-    }
+    private boolean currentBlobSelected;
     
     /**
      * load all blobs uploaded in the past month into memory
      * this is temp for testing, to be replaced by proper query search implementation
      */
     @PostConstruct
-    public void intiBean(){
+    public void initBean(){
+        pageModes = new ArrayList<>();
+        pageModes.add(PageModeEnum.LOOKUP);
+        pageModes.add(PageModeEnum.INSERT);
+        pageModes.add(PageModeEnum.UPDATE);
+        pageModes.add(PageModeEnum.REMOVE);
+        
+        setCurrentMode(PageModeEnum.LOOKUP);
+        
         try {
             BlobIntegrator bi = getBlobIntegrator();
             blobList = new ArrayList<>();
             List<Integer> blobIDs = bi.getRecentPhotoBlobs();
             for (int idnum : blobIDs) {
-                //blobList.add(bi.get)
+                blobList.add(bi.getPhotoBlobLight(idnum));
             }
             
-        } catch (IntegrationException ex) {
-            System.out.println("manageBlobBB.initBean | " + ex + "\n" + ex.getException().getLocalizedMessage());
+        } catch (IntegrationException | ClassNotFoundException | IOException ex) {
+            System.out.println("manageBlobBB.initBean | ERROR: " + ex);
         }
     }
     
@@ -104,20 +123,6 @@ public class manageBlobBB extends BackingBeanUtils implements Serializable{
     }
 
     /**
-     * @return the blobList
-     */
-    public List<Blob> getBlobList() {
-        return blobList;
-    }
-
-    /**
-     * @param blobList the blobList to set
-     */
-    public void setBlobList(List<Blob> blobList) {
-        this.blobList = blobList;
-    }
-
-    /**
      * @return the selectedBlob
      */
     public Blob getSelectedBlob() {
@@ -129,6 +134,65 @@ public class manageBlobBB extends BackingBeanUtils implements Serializable{
      */
     public void setSelectedBlob(Blob selectedBlob) {
         this.selectedBlob = selectedBlob;
+    }
+
+    //check if current mode == Lookup
+    public boolean getActiveLookupMode() {
+        // hard-wired on since there's always a property loaded
+        return PageModeEnum.LOOKUP.equals(currentMode);
+    }
+
+    //check if current mode == Lookup
+    public boolean getActiveViewMode() {
+        return PageModeEnum.VIEW.equals(currentMode) || PageModeEnum.LOOKUP.equals(currentMode);
+
+    }
+
+    //check if current mode == Insert
+    public boolean getActiveInsertUpdateMode() {
+        return PageModeEnum.INSERT.equals(currentMode) || PageModeEnum.UPDATE.equals(currentMode);
+    }
+
+    //check if current mode == Remove
+    public boolean getActiveRemoveMode() {
+        return PageModeEnum.REMOVE.equals(currentMode);
+    }
+    
+    public PageModeEnum getCurrentMode() {
+        return currentMode;
+    }
+
+    public void setCurrentMode(PageModeEnum input) {
+        if(input != null){
+        currentMode = input;
+        getFacesContext().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO,
+                            this.currentMode.getTitle() + " Mode Selected", ""));
+        }
+    }
+
+    public List<PageModeEnum> getPageModes() {
+        return pageModes;
+    }
+
+    public void setPageModes(List<PageModeEnum> pageModes) {
+        this.pageModes = pageModes;
+    }
+
+    public List<BlobLight> getBlobList() {
+        return blobList;
+    }
+
+    public void setBlobList(List<BlobLight> blobList) {
+        this.blobList = blobList;
+    }
+
+    public boolean isCurrentBlobSelected() {
+        return currentBlobSelected;
+    }
+
+    public void setCurrentBlobSelected(boolean currentBlobSelected) {
+        this.currentBlobSelected = currentBlobSelected;
     }
     
 }
