30ec76006af5da465b49cf17defaf1b32d20dff9
Eric Darsow <ericdarsow@gmail.com>
2021-07-20 18:08:27 -0400

    db patch 35.1 ready for implementation in java:run CREATE TYPE alone

---
+++ codeconnect/database/patches/dbpatch_beta35.1.sql
@@ -0,0 +1,179 @@
+-- Occupancy inspection refactor for inspeection UI by JURPLEL
+
+
+ALTER TABLE public.occspaceelement
+	ADD COLUMN occpacetype_typeid INTEGER CONSTRAINT occpsacelement_typeid_fk REFERENCES public.occspacetype (spacetypeid);
+
+ALTER TABLE public.occspaceelement DROP COLUMN space_id;
+
+DROP TABLE public.occspace CASCADE;
+
+ALTER TABLE public.occchecklistspacetype
+	DROP COLUMN overridespacetyperequired;
+
+ALTER TABLE public.occchecklistspacetype
+	DROP COLUMN overridespacetyperequiredvalue;
+
+ALTER TABLE public.occchecklistspacetype
+	DROP COLUMN overridespacetyperequireallspaces;
+
+ALTER TABLE public.occchecklistspacetype
+	ADD COLUMN notes TEXT;
+
+ALTER TABLE public.occinspectedspace
+	DROP COLUMN occspace_spaceid;
+
+ALTER TABLE public.occinspectedspace
+	ADD COLUMN occchecklistspacetype_chklstspctypid INTEGER 
+	CONSTRAINT occinspectedspace_chklstspctypid_fk REFERENCES public.occchecklistspacetype (checklistspacetypeid);
+
+ALTER TABLE public.occinspectedspaceelement
+	ADD COLUMN migratetocecaseonfail BOOLEAN DEFAULT TRUE;
+
+
+
+
+ALTER TABLE public.occinspection 
+	ADD COLUMN followupto_inspectionid INTEGER 
+	CONSTRAINT occinspection_followupto_fk REFERENCES occinspection (inspectionid);
+
+ALTER TABLE public.occinspection
+	RENAME COLUMN notes TO notespreinspection;
+
+ALTER TABLE public.occinspection ADD COLUMN timestart TIMESTAMP WITH TIME ZONE;
+ALTER TABLE public.occinspection ADD COLUMN timeend TIMESTAMP WITH TIME ZONE;
+
+
+
+CREATE TABLE public.occinspectionpropertystatus
+(
+	occinspection_inspectionid INTEGER NOT NULL,
+	propertystatus_statusid 	INTEGER NOT NULL,
+	notes 						TEXT,
+	CONSTRAINT occinspectionpropstatuspk_comp PRIMARY KEY (occinspection_inspectionid, propertystatus_statusid),
+	CONSTRAINT occinspectionpropertystatus_inspection_fk FOREIGN KEY (occinspection_inspectionid) 
+		REFERENCES occinspection (inspectionid),
+	CONSTRAINT occinspectionpropertystatus_status_fk FOREIGN KEY (propertystatus_statusid)
+		REFERENCES propertystatus (statusid)
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS occinspectioncause_causeid_seq
+    START WITH 100
+    INCREMENT BY 1
+    MINVALUE 100
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.occinspectioncause
+(
+	causeid 		INTEGER PRIMARY KEY DEFAULT nextval('occinspectioncause_causeid_seq'),
+	title 			TEXT NOT NULL,
+	description 	TEXT,
+	notes 			TEXT
+);
+
+ALTER TABLE public.occinspection 
+	ADD COLUMN cause_causeid INTEGER 
+	CONSTRAINT occinspection_cause_fk REFERNECES occinspectioncause (causeid);
+
+
+
+
+
+CREATE SEQUENCE IF NOT EXISTS occinspection_determination_seq
+    START WITH 100
+    INCREMENT BY 1
+    MINVALUE 100
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.occinspectiondetermination
+(
+	determinationid 		INTEGER PRIMARY KEY DEFAULT nextval('occinspection_determination_seq'),
+	title 			TEXT NOT NULL,
+	description 	TEXT,
+	notes 			TEXT,
+	eventcat_catid 	INTEGER CONSTRAINT occinspectiondetermination_eventcat_fk REFERENCES eventcategory (categoryid)
+);
+
+ALTER TABLE public.occinspection 
+	ADD COLUMN determination_detid INTEGER 
+	CONSTRAINT occinspection_determination_fk REFERENCES occinspectiondetermination (determinationid);
+
+ALTER TABLE public.occinspection 
+	ADD COLUMN determinationby_userid INTEGER
+	CONSTRAINT occinpection_determinationby_fk REFERENCES login (userid);
+
+ALTER TABLE public.occinspection
+	ADD COLUMN determinationts TIMESTAMP WITH TIME ZONE;
+
+
+
+
+ALTER TABLE public.occinspection DROP COLUMN passedinspection_userid;
+ALTER TABLE public.occinspection DROP COLUMN passedinspectionts;
+
+ALTER TABLE public.occinspection ADD COLUMN remarks TEXT;
+ALTER TABLE public.occinspection ADD COLUMN generalcomments TEXT;
+
+
+CREATE SEQUENCE IF NOT EXISTS occchecklist_photorequirement_seq
+    START WITH 100
+    INCREMENT BY 1
+    MINVALUE 100
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.occphotorequirement
+(
+	requirementid 		INTEGER PRIMARY KEY DEFAULT nextval('occchecklist_photorequirement_seq'),
+	title 			TEXT NOT NULL,
+	description 	TEXT,
+	notes 			TEXT,
+	required 		BOOLEAN
+);
+
+CREATE TABLE public.occchecklistphotorequirement
+(
+	occchecklist_checklistid 	INTEGER NOT NULL,
+	occphotorequirement_reqid 	INTEGER NOT NULL,
+	CONSTRAINT occchecklistphotorequirement_pk_comp PRIMARY KEY (occchecklist_checklistid, occphotorequirement_reqid),
+	CONSTRAINT occchecklistphotorequirement_checklist_fk FOREIGN KEY 
+		REFERENCES public.occchecklist (checklistid),
+	CONSTRAINT occchecklistphotorequirement_requirement_fk FOREIGN KEY 
+		REFERENCES public.occphotorequirement (requirementid)
+
+);
+
+CREATE TABLE public.occinspectionphotodoc
+(
+  photodoc_photodocid integer NOT NULL,
+  inspection_inspectionid integer NOT NULL,
+  photorequirement_requirementid INTEGER,
+  CONSTRAINT occinspectionphotodoc_pk_comp PRIMARY KEY (photodoc_photodocid, inspection_inspectionid),
+  CONSTRAINT occinspectionphotodoc_inspection_fk FOREIGN KEY (inspection_inspectionid)
+      REFERENCES public.occinspection (inspectionid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT occinspectionphotodoc_phtodoc_fk FOREIGN KEY (photodoc_photodocid)
+      REFERENCES public.photodoc (photodocid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT occinpectionphotodoc_requirement_fk FOREIGN KEY (photorequirement_requirementid)
+  		REFERENCES public.occphotorequirement (requirementid)
+);
+
+DROP TABLE public.occinspectedspaceelementpdfdoc;
+
+-- ****************************************************************************
+-- RUN THIS CREATE TYPE BY ITSELF!
+-- CREATE TYPE occinspectionphototype AS ENUM ('PassDocumentation','FailDocumentation','GeneralDocumentation','Other','Unused');
+
+-- ****************************************************************************
+
+ALTER TABLE public.occinspectedspaceelementphotodoc ADD COLUMN phototype occinspectionphototype;
+
+-- RUN LOCALLY UP TO HERE
+
+--IF datepublished IS NULL the patch is still open and receiving changes
+INSERT INTO public.dbpatch(patchnum, patchfilename, datepublished, patchauthor, notes)
+    VALUES (351, 'database/patches/dbpatch_beta35.1.sql', '07-19-2021', 'ecd', 'Occ refactor: collapse occspace and occspacetype, revise occinspection');
