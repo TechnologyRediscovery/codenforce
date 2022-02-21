9e241ea04fe8c15f24808e7b987a1803e325f72c
Eric Darsow <ericdarsow@gmail.com>
2022-02-21 18:02:37 -0500

    dispatch table schema

---
+++ codeconnect/database/patches/dbpatch_mobilephase1.sql
@@ -0,0 +1,26 @@
+
+-- database patch for creating the database infrastructure to allow
+-- offile mobile completion of occupancy inspection 
+
+-- thanks to CHEN&CHEN!!
+
+CREATE SEQUENCE IF NOT EXISTS occinspectiondispatch_dispatchid_seq
+    START WITH 100
+    INCREMENT BY 1
+    MINVALUE 100
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.occinspectiondispatch
+(
+	dispatchid 			INTEGER PRIMARY KEY DEFAULT nextval('occinspectiondispatch_dispatchid_seq'),
+	createdby_userid	INTEGER NOT NULL CONSTRAINT occinspectiondispatch_createdby_userid_fk REFERENCES login (userid),
+	creationts 			TIMESTAMP WITH TIME ZONE NOT NULL,
+	dispatchnotes		TEXT,
+	inspection_inspectionID INTEGER NOT NULL CONSTRAINT occinspectiondispatch_occinspectionid_fk REFERENCES occinspection (inspeectionid),
+	retrievalts			TIMESTAMP WITH TIME ZONE,
+	retrievedby_userid	INTEGER CONSTRAINT occinspectiondispatch_retrievedby_userid_fk REFERENCES login (userid),
+	synchronizationts 	TIMESTAMP WITH TIME ZONE,
+	synchronizationnotes TEXT
+);
+
